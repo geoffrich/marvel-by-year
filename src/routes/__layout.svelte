61e25ecb33e3bbd786e588275c34d53a61f0ed51
Geoff Rich <geoff.rich@alaskaair.com>
2021-09-10 13:05:27 -0700

    Add loading indicator

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import PreloadingIndicator from '$lib/components/PreloadingIndicator.svelte';
+	import { navigating } from '$app/stores';
+</script>
+
+{#if $navigating}
+	<PreloadingIndicator />
+{/if}
+
 <nav>
 	<a href="/">Home</a>
 </nav>
