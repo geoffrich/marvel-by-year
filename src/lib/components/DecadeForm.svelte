c17b79f3ed962940b8dcfac28b16dfaef52214da
Geoff Rich <geoff.rich@alaskaair.com>
2021-09-25 11:09:02 -0700

    Submit form client side

--- src/lib/components/DecadeForm.svelte
+++ src/lib/components/DecadeForm.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import Select from '$lib/components/form/Select.svelte';
 	import { decades } from '$lib/years';
 
+	export let selected: string | number;
+
 	const options = ['Select a decade', ...decades.map((d) => d.text)];
 	const values = ['', ...decades.map((d) => d.startYear)];
-	export let selected: string | number;
+	const action = '/comic/random';
+	let form: HTMLFormElement;
+
+	function submit() {
+		const formData = new FormData(form);
+		goto(`${action}?decade=${formData.get('decade')}`, { keepfocus: true });
+	}
+
+	function handleEnter(e: KeyboardEvent) {
+		if (e.key === 'Enter') {
+			submit();
+		}
+	}
 </script>
 
-<!-- TODO: submit client-side if JS available -->
-<form action="/comic/random">
+<form bind:this={form} {action} on:submit|preventDefault={submit} on:keydown={handleEnter}>
 	<div>
 		<Select {options} {values} id="decade" name="decade" value={selected ? selected : ''}
 			>Decade</Select
