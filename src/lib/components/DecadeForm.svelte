0538127534ac07f087022f41487ff637ab922388
Geoff Rich <geoff.rich@alaskaair.com>
2021-09-25 12:09:31 -0700

    No scroll

--- src/lib/components/DecadeForm.svelte
+++ src/lib/components/DecadeForm.svelte
@@ -5,14 +5,14 @@
 
 	export let selected: string | number;
 
-	const options = ['Select a decade', ...decades.map((d) => d.text)];
+	const options = ['All decades', ...decades.map((d) => d.text)];
 	const values = ['', ...decades.map((d) => d.startYear)];
 	const action = '/comic/random';
 	let form: HTMLFormElement;
 
 	function submit() {
 		const formData = new FormData(form);
-		goto(`${action}?decade=${formData.get('decade')}`, { keepfocus: true });
+		goto(`${action}?decade=${formData.get('decade')}`, { keepfocus: true, noscroll: true });
 	}
 
 	function handleEnter(e: KeyboardEvent) {
