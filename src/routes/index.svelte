7e7fea0be82d8e33ad5ae16b206ff4cbb626bc95
Geoff Rich <geoff.rich@alaskaair.com>
2021-09-25 20:38:46 -0700

    Update homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,27 +4,57 @@
 
 <script>
 	import title from '$lib/stores/title';
+	import { MIN_YEAR, MAX_YEAR } from '$lib/years';
 
 	$title = 'Home';
 </script>
 
-<h1>Browse Marvel Unlimited by Year</h1>
-<p>
-	This site allows you to see what issues are available on Marvel Unlimited for a given year. Tap
-	the comic's cover to be taken directly to the comic in Marvel Unlimited or the web-based reader.
-</p>
-<p>
-	You can also filter the available issues for a given year by series, creators, or events. Note
-	that the data is paginated&mdash;you may not see all series for a given year until you have
-	navigated to further pages.
-</p>
-<p>
-	This site is a work in progress. Due to limitations of the Marvel API, you may experience issues
-	such as timeouts and rate-limiting. Marvel limits developers to 3000 API calls/day&mdash;caching
-	is implemented to try and mitigate that, but it still may be an issue. If you can't get a page to
-	load, try again tomorrow.
-</p>
-<p>
-	Because of these API limits, please <em>do not</em> share links to this site on social media for the
-	time being.
-</p>
+<div class="container">
+	<h1>Browse Marvel Unlimited by Year</h1>
+	<p>
+		This site allows you to see what issues are available on Marvel Unlimited for a given year. Tap
+		the comic's cover to be taken directly to the comic in Marvel Unlimited or the web-based reader.
+		You can also filter the available issues for a given year by series, creators, or events.
+	</p>
+
+	<ul>
+		<li><a href="/year/{MIN_YEAR}">Start at the beginning</a></li>
+		<li><a href="/year/{MAX_YEAR}">See what's current</a></li>
+		<li><a href="/comic/random">Choose a comic for me</a></li>
+		<li><a href="/year/1965?search=Daredevil%208">Stilts be praised</a></li>
+	</ul>
+</div>
+
+<style>
+	p {
+		margin-bottom: 2rem;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		gap: 1rem;
+		align-content: stretch;
+	}
+
+	a {
+		color: white;
+		background-color: var(--primary);
+		padding: 3rem 1rem;
+		text-align: center;
+		font-size: 1.25rem;
+		display: block;
+		border-radius: 0.5rem;
+		height: 100%;
+		text-decoration: none;
+	}
+
+	a:hover:not(:active),
+	a:focus {
+		color: var(--primary);
+		background-color: var(--secondary);
+	}
+</style>
